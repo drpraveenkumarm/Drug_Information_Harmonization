f21b7283c27abe228005e806d08ce5b9b082e78c
drpraveenkumarm <79145894+drpraveenkumarm@users.noreply.github.com>
2022-05-17 21:14:15 +0530

    added order_dose_amount and order_frequency

--- explore_dose.py
+++ explore_dose.py
@@ -4,6 +4,14 @@ sp = SparkSQLAPI(debug=True,data_version='MayoCDAPDev')
 
 drug_name = "propranolol"
 
-df_order_dose_amount_freq = sp.query(f"""select cast(order_dose_amount as decimal(9,2)), count(*) as value 
+df_order_dose_amount_count = sp.query(f"""select cast(order_dose_amount as decimal(9,2)), count(*) as value 
 from  fact_orders where drug_names like ("%{drug_name}%") group by order_dose_amount order 
 by value DESC""")
+
+df_order_dose_amount_count
+
+df_order_frequency = sp.query(f"""select order_frequency, count(*) as value 
+from  fact_orders where drug_names like ("%{drug_name}%") group by order_frequency order 
+by value DESC""")
+
+df_order_frequency
