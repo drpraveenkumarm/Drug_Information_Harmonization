9a916855d2cd40e7a862907d9f4396767250d8ce
drpraveenkumarm <79145894+drpraveenkumarm@users.noreply.github.com>
2022-05-17 21:09:52 +0530

    Update explore_dose.py

--- explore_dose.py
+++ explore_dose.py
@@ -4,6 +4,6 @@ sp = SparkSQLAPI(debug=True,data_version='MayoCDAPDev')
 
 drug_name = "propranolol"
 
-df_out = sp.query(f"""select cast(order_dose_amount as decimal(9,2)), count(*) as value 
+df_order_dose_amount_freq = sp.query(f"""select cast(order_dose_amount as decimal(9,2)), count(*) as value 
 from  fact_orders where drug_names like ("%{drug_name}%") group by order_dose_amount order 
 by value DESC""")
