74f8abb068702fcd79fb7494ead423a348852132
drpraveenkumarm <79145894+drpraveenkumarm@users.noreply.github.com>
2022-05-17 21:08:31 +0530

    Update explore_dose.py

--- explore_dose.py
+++ explore_dose.py
@@ -1,1 +1,9 @@
+from nferx_sdk.data_sources import SparkSQLAPI
 
+sp = SparkSQLAPI(debug=True,data_version='MayoCDAPDev')
+
+drug_name = "propranolol"
+
+df_out = sp.query(f"""select cast(order_dose_amount as decimal(9,2)), count(*) as value 
+from  fact_orders where drug_names like ("%{drug_name}%") group by order_dose_amount order 
+by value DESC""")
